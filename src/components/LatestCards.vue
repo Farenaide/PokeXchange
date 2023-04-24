b27daf48a3287c1c83cab0c303f3f2e03702d456
Alan <aalan.lima@live.com>
2023-04-24 18:12:26 -0300

    Refactor LatestCards component name

---
+++ src/components/LatestCards.vue
@@ -0,0 +1,73 @@
+<template>
+    <article>
+        <h3>Latest Cards</h3>
+        <ul>
+            <li v-for="card in LatestCards" :key="card.cardId">
+                <img :src="card.cardImage" :alt="card.cardName">
+            </li>
+        </ul>
+    </article>
+</template>
+
+<script>
+
+    import apiTCG from '@/services/apiTCG.js'
+
+    export default {
+        name: 'LatestCards',
+        data(){
+            return{
+                LatestCards: 'Loading...'
+            }
+        },
+        methods: {
+            randomNumber(min, max) {
+                return Math.random() * (max - min) + min;
+            }
+        },
+        mounted(){
+            apiTCG.get('/cards', {
+                params: {                    
+                    pageSize: 10, // número de cartas por página
+                    page: this.randomNumber(1, 1600) // número da página
+                }
+            }).then(response =>{
+                const cardsArr = [...response.data.data]
+                const totalArr = []
+                cardsArr.map(card=>{
+                    const cardName = card.name
+                    const cardId = card.id
+                    const cardImage = card.images.small
+                    totalArr.push({cardId, cardName, cardImage})
+                })
+                this.LatestCards = totalArr  
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    article{
+        max-width: 1440px;
+        margin: 60px auto 0;
+        padding: 0 20px;
+    }
+    h3{
+        font-family: var(--font-Noto-Sans); 
+        font-size: 30px;
+        font-weight: 700;
+        margin-bottom: 40px;
+    }
+    ul{
+        display: flex;        
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    ul li{
+        margin-bottom: 50px;
+    }
+    img{
+        box-sizing: border-box;
+        border-radius: 20px;
+    }
+</style>
