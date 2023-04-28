e7ba1a0a7b59b23652d4a3fdee1273f507c1d96e
Alan <aalan.lima@live.com>
2023-04-28 13:35:21 -0300

    Refactor searchName filter for better way

--- src/components/FilterMarket.vue
+++ src/components/FilterMarket.vue
@@ -3,23 +3,58 @@
         <label class="search-name" for="search-name">
             <i class="fa-solid fa-magnifying-glass"></i>
             <input
-            :value="searchName"
-            @change="$emit('update:searchName', $event.target.value)"
+            class="search-name-input"
+            v-model="searchName"
+            @change="$emit('changeSearchName', searchName)"
             id="search-name" 
             type="text" 
             placeholder="e.g. Pikachu"
             />
-        </label>  
+        </label> 
+
+        <div class="filter-types">
+            <label :for="element" v-for="element in types" :key="element" >
+                <img :src="`../../public/types/${element}.png`" :alt="element">
+                <p>{{ element }}</p>
+                <input 
+                type="checkbox" 
+                name="type" 
+                :id="element"
+                :true-value="typeFilter"
+                :false-value="typeFilter"
+                @click="$emit('update:typeFilter', `${element}`)"
+                />
+            </label>
+        </div>
     </nav>
 </template>
 
 <script>
+    import { ref } from 'vue'
+    import apiTCG from '@/services/apiTCG.js'
+
     export default {
         name: 'FilterMarket',
         props: {
-            searchName: String
+            typeFilter: String
         },
-        emits: ['update:searchName']
+        components:{
+        }
+        ,        
+        emits: ['changeSearchName'],
+        setup(){
+            const types = ref([])
+            const searchName = ref('')
+
+            apiTCG.get('/types').then((response) => {
+                types.value = response.data.data
+            }); 
+
+            return{
+                types,
+                searchName
+            }
+        }
     }
 </script>
 
@@ -40,14 +75,14 @@
         align-items: center;
         height: 30px;
         width: 100%;
-        padding: 0 20px;
+        border: 1px dotted red;
     }
     .fa-solid{
         margin-right: 10px;
         font-size: 20px;
         cursor: pointer;
     }
-    input {
+    .search-name-input {
         flex-grow: 1;        
         box-sizing: border-box;
         outline: none;
@@ -60,5 +95,31 @@
         padding: 0 15px;
         color: var(--color-white);
     }
+    .filter-types{
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(110px, 1fr));
+        gap: 5px;
+        border: 1px dotted red;
+        box-sizing: border-box;
+        margin: 30px auto 0;
+    }
+    .filter-types label{
+        display: flex;
+        width: 110px;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 15px;
+        font-family: var(--font-Roboto);
+        font-weight: 500;
+        cursor: pointer;
+        margin: 0 auto;
+    }
+    .filter-types label img {
+        height: 15px;
+        margin-right: 5px;
+    }
+
+
 </style>
 
