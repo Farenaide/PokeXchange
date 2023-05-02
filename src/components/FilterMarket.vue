6684e2aa3f52436fccd0dd7e8e8434295a6cfc85
Alan <aalan.lima@live.com>
2023-05-02 19:50:54 -0300

    Add filter by rarities in FilterMarket component, and refactor supertypes visual

--- src/components/FilterMarket.vue
+++ src/components/FilterMarket.vue
@@ -19,20 +19,27 @@
             $emit('changeSubtype', selectedSubtype = '*') ,
             $emit('changeSearchName', searchName = ''),
             $emit('changeHp', ''),
+            $emit('changeRarity', selectedRarity = '*')
             ]"
         >
-            <label v-for="supertype in allSuperTypes" :key="supertype">
+            <div v-for="supertype in allSuperTypes" :key="supertype">
                 <input class="search-supertype-input"
                 type="radio" 
                 name="superTypes" 
                 :id="supertype" 
                 :value="supertype" 
                 v-model="selectedSuperType">
-                <p>{{ supertype }}</p>
-            </label>
+                <label :for="supertype">
+                    <div class="wrapper-border">
+                        <img :src="`/public/${supertype}Select.png`" :alt="supertype">
+                        <div :class="`${supertype} select`"></div>
+                        <div :class="`${supertype}2 select2`"></div>
+                    </div>
+                </label>
+            </div>
         </div>
         
-        <div class="filter-subtypes">
+        <div class="filter-select">
             <select v-model="selectedSubtype" @change="$emit('changeSubtype', selectedSubtype)">
                 <option value="*">All Subtypes</option>
                 <option 
@@ -46,10 +53,23 @@
             </select>
         </div>
 
+        <div class="filter-select">
+            <select v-model="selectedRarity" @change="$emit('changeRarity', selectedRarity)">
+                <option value="*">All Rarities</option>
+                <option 
+                v-for="Rarity in rarities" 
+                :key="Rarity" 
+                :value="Rarity"
+                id="allOptions"
+                >
+                    <div>{{ Rarity }}</div>
+                </option>
+            </select>
+        </div>
+
         <div class="filter-types"
-            v-if="selectedSuperType === 'Pokémon'"
-            @change="$emit('changeType', selectedTypes.join(' '))"
-        >
+        v-if="selectedSuperType === 'Pokémon'"
+        @change="$emit('changeType', selectedTypes.join(' '))">
             <div class="input-wrapper"
             v-for="element in allTypes" :key="element"> 
                 <input   
@@ -68,8 +88,7 @@
         </div>
         
         <div class="filter-hp"
-        v-if="(selectedSuperType === 'Trainer' || selectedSuperType === 'Pokémon')"
-        >
+        v-if="(selectedSuperType === 'Trainer' || selectedSuperType === 'Pokémon')">
             <p>Min HP {{ sliderMin }}</p>
             <p>Max HP {{ sliderMax }}</p>
             <input 
@@ -79,7 +98,6 @@
             @input="$emit('changeHp', `hp:[${sliderMin} TO ${sliderMax}]`)"
             class="filter-max" type="range" :min="minHP" :max="maxHP" step="10" v-model="sliderMax"> 
         </div>
-        
     </nav>
 </template>
 
@@ -94,15 +112,21 @@
             'changeType',
             'changeSuperType',
             'changeSubtype',
-            'changeHp'
+            'changeHp',
+            'changeRarity'
         ],
         setup(){
             const allTypes = myData.types
             const allSuperTypes = myData.supertypes
             const trainerSubtypes = myData.subtypes.trainerSubtypes
             const pokemonSubtypes = myData.subtypes.pokemonSubtypes
+            const trainerRarities = myData.rarity.trainerRarities
+            const energyRarities = myData.rarity.energyRarities
+            const pokemonRarities = myData.rarity.pokemonRarities
             const energySubtypes = myData.subtypes.energySubtypes
             const subtypes = ref(pokemonSubtypes)
+            const rarities = ref(pokemonRarities)
+
 
             const maxHP = ref(340)
             const minHP = ref(0)
@@ -111,6 +135,7 @@
             const selectedTypes = ref([])
             const selectedSubtype = ref('*')
             const selectedSuperType = ref('Pokémon')
+            const selectedRarity = ref('*')
             const sliderMin = ref(minHP.value)
             const sliderMax = ref(maxHP.value)
 
@@ -135,15 +160,18 @@
                 switch (selectedSuperType.value) {
                     case 'Pokémon':
                         subtypes.value = pokemonSubtypes
+                        rarities.value = pokemonRarities
                         sliderMax.value = maxHP.value = 340
                         sliderMin.value = 0
                         break;
                     case 'Trainer':
+                        rarities.value = trainerRarities
                         subtypes.value = trainerSubtypes
                         sliderMax.value = maxHP.value = 60 
                         sliderMin.value = 0
                         break
                     case 'Energy':
+                        rarities.value = energyRarities
                         subtypes.value = energySubtypes
                         break
                     default:
@@ -159,16 +187,18 @@
                 selectedTypes,
                 selectedSubtype,
                 selectedSuperType,
+                selectedRarity,
                 sliderMin,
                 sliderMax,
                 maxHP,
-                minHP
+                minHP,
+                rarities
             }
         }
     }
 </script>
 
-<style scoped>
+<style scoped> /* NAV */
     nav{
         box-sizing: border-box;
         max-width: 380px;
@@ -179,73 +209,11 @@
         height: 85vh;
     }
 
-    .search-name{
-        box-sizing: border-box;
-        display: flex;
-        align-items: center;
-        height: 40px;
-        width: 100%;
-        position: relative;
-        border: 1px solid var(--color-white);
-    }
-    .fa-solid{
-        margin-right: 10px;
-        font-size: 20px;
-        cursor: pointer;
-        position: absolute;
-        left: 10px;
-    }
-    .search-name-input {
-        flex-grow: 1;        
-        box-sizing: border-box;
-        outline: none;
-        border: none;
-        width: 0;
-        min-width: 100px;
-        height: 100%;
-        background-color: var(--color-first--variant);
-        padding: 0 15px;
-        padding-left: 40px;
-        color: var(--color-white);
-    }
 
-    .search-name-input:focus{
-        outline: 2px solid var(--color-third);
-    }
-    
-    .filter-supertypes{
-        display: flex;
-        flex-direction: column;
-        gap: 15px;
-        margin: 20px auto 0;
-        margin-left: 0;
-        cursor: pointer;
-    }
+</style>
 
-    .filter-supertypes label{
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-    }
-
-    .search-supertype-input{
-        appearance: none;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        margin: 0;
-        margin-right: 5px;
-        margin-bottom: 2px;
-        height: 15px;
-        width: 15px;
-        border-radius: 50%;
-        background-color: var(--color-white);
-        cursor: pointer;
-    }
-    .search-supertype-input:checked{
-        background-color: var(--color-third);
-    }
-
-    .filter-subtypes{
+<style scoped> /* Filter Select */
+    .filter-select{
         box-sizing: border-box;
         margin-top: 20px;
         width: 100%;
@@ -253,7 +221,7 @@
         position: relative;
     }
 
-    .filter-subtypes::after{
+    .filter-select::after{
         content: '\25B8';
         display: block;
         height: 50%;
@@ -261,8 +229,9 @@
         right: 10px;
         top: 25%;
         rotate: 90deg;
+        pointer-events: none;
     }
-    .filter-subtypes select {
+    .filter-select select {
         appearance: none;
         -webkit-appearance: none;
         -moz-appearance: none;
@@ -276,14 +245,184 @@
         user-select: none;
         border: 1px solid var(--color-white);
     }
-    .filter-subtypes select:focus {
+    .filter-select select:focus {
         outline: none;
         border: none;
         border: 2px solid var(--color-third);
         
     }
 
-    .label-types img {
+</style>
+
+<style scoped> /* Search Name */
+    .search-name{
+            box-sizing: border-box;
+            display: flex;
+            align-items: center;
+            height: 40px;
+            width: 100%;
+            position: relative;
+            border: 1px solid var(--color-white);
+        }
+        .fa-solid{
+            margin-right: 10px;
+            font-size: 20px;
+            cursor: pointer;
+            position: absolute;
+            left: 10px;
+        }
+        .search-name-input {
+            flex-grow: 1;        
+            box-sizing: border-box;
+            outline: none;
+            border: none;
+            width: 0;
+            min-width: 100px;
+            height: 100%;
+            background-color: var(--color-first--variant);
+            padding: 0 15px;
+            padding-left: 40px;
+            color: var(--color-white);
+        }
+
+        .search-name-input:focus{
+            outline: 2px solid var(--color-third);
+        }
+    
+</style>
+
+<style scoped> /* Supertype */
+    .filter-supertypes{
+            display: flex;
+            margin: 20px auto 0;
+            margin-left: 0;
+            cursor: pointer;
+            width: 100%;
+            gap: 10px;
+        }
+    .filter-supertypes label{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+    }
+    .search-supertype-input{
+        display: none;
+    }
+    .wrapper-border{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        position: relative;
+        padding: 5px;
+    }
+    .wrapper-border img{
+        width: 100%;
+        height: 100%;
+    }
+
+    .search-supertype-input:checked + label .select:before,
+    .search-supertype-input:checked + label .select:after,
+    .search-supertype-input:checked + label .select2:before,
+    .search-supertype-input:checked + label .select2:after{
+        content: "";
+        position: absolute;
+        width: 10px;
+        height: 10px;
+    }
+
+    #Pokémon:checked + label .Pokémon:before {
+        top: 0;
+        left: 0;
+        border-top: 2px solid;
+        border-left: 2px solid;
+        border-color: #e5961d;
+    }
+    #Pokémon:checked + label .Pokémon::after {
+        bottom: 0;
+        left: 0;
+        border-bottom: 2px solid;
+        border-left: 2px solid;
+        border-color: #e5961d;
+    }
+    #Pokémon:checked + label .Pokémon2:before {
+        top: 0;
+        right: 0;
+        border-top: 2px solid;
+        border-right: 2px solid;
+        border-color: #e5961d;
+    }
+    #Pokémon:checked + label .Pokémon2:after {
+        bottom: 0;
+        right: 0;
+        border-bottom: 2px solid;
+        border-right: 2px solid;
+        border-color: #e5961d;
+    }
+
+    #Trainer:checked + label .Trainer:before {
+        top: 0;
+        left: 0;
+        border-top: 2px solid;
+        border-left: 2px solid;
+        border-color: #cdbf9a;
+    }
+    #Trainer:checked + label .Trainer::after {
+        bottom: 0;
+        left: 0;
+        border-bottom: 2px solid;
+        border-left: 2px solid;
+        border-color: #cdbf9a;
+    }
+    #Trainer:checked + label .Trainer2:before {
+        top: 0;
+        right: 0;
+        border-top: 2px solid;
+        border-right: 2px solid;
+        border-color: #cdbf9a;
+    }
+    #Trainer:checked + label .Trainer2:after {
+        bottom: 0;
+        right: 0;
+        border-bottom: 2px solid;
+        border-right: 2px solid;
+        border-color: #cdbf9a;
+    }
+
+    #Energy:checked + label .Energy:before {
+        top: 0;
+        left: 0;
+        border-top: 2px solid;
+        border-left: 2px solid;
+        border-color: #f5f5f7;
+    }
+    #Energy:checked + label .Energy::after {
+        bottom: 0;
+        left: 0;
+        border-bottom: 2px solid;
+        border-left: 2px solid;
+        border-color: #f5f5f7;
+    }
+    #Energy:checked + label .Energy2:before {
+        top: 0;
+        right: 0;
+        border-top: 2px solid;
+        border-right: 2px solid;
+        border-color: #f5f5f7;
+    }
+    #Energy:checked + label .Energy2:after {
+        bottom: 0;
+        right: 0;
+        border-bottom: 2px solid;
+        border-right: 2px solid;
+        border-color: #f5f5f7;
+    }
+</style>
+
+<style scoped> /* Types */
+        .label-types img {
         width: 15px;
         filter: brightness(1.5);
         margin-right: 5px;
@@ -312,78 +451,6 @@
         background-color: var(--color-first--variant);
         border-radius: 8px;
     }
-
-</style>
-
-<style scoped>
-    .filter-hp{
-        margin-top: 20px;
-        width: 100%;
-        position: relative;
-        display: flex;
-    }
-    .filter-hp p {
-        width: 100%;
-        color: var(--color-second);
-    }
-    .filter-hp p:nth-child(2){
-        text-align: end;
-        color: var(--color-third);
-    }
-    .filter-hp input{
-        -webkit-appearance: none;  
-        appearance: none;
-        width: 100%; 
-        height: 10px; 
-        background: #1c1c36; 
-        border-radius: 5px;
-        position: absolute;
-        pointer-events: none;
-        opacity: 1;
-        top: 20px;
-    }
-    .filter-hp input:focus{
-        outline: none;
-    }
-    .filter-hp .filter-min{
-        width: 92%;
-    }
-
-    .filter-hp .filter-max{
-        width: 92%;
-        right: 0;
-    }
-    .filter-hp input::-webkit-slider-thumb {
-        -webkit-appearance: none; 
-        appearance: none;
-        width: 15px; 
-        height: 15px; 
-        background: var(--color-third); 
-        filter: opacity(.7);
-        cursor: pointer;
-        border-radius: 50%;
-        -webkit-transition: .1s; 
-        transition: border .1s;
-        position: relative;
-        z-index: 1;
-        outline: 2px solid var(--color-first);
-        pointer-events: all;
-    }
-
-    .filter-hp .filter-min::-webkit-slider-thumb{
-        background-color: var(--color-second);
-    }
-
-    .filter-hp input::-webkit-slider-thumb:hover {
-        filter: opacity(1); 
-        scale: 1.1; 
-    }
-
-
-
-</style>
-
-<style scoped>
     .filter-Colorless + label{
         color: #fff;
     }
@@ -472,4 +539,72 @@
         filter: contrast(1.2);
     }
     
+</style>
+
+<style scoped> /* HealthPoints */
+    .filter-hp{
+        margin-top: 20px;
+        width: 100%;
+        position: relative;
+        display: flex;
+    }
+    .filter-hp p {
+        width: 100%;
+        color: var(--color-second);
+    }
+    .filter-hp p:nth-child(2){
+        text-align: end;
+        color: var(--color-third);
+    }
+    .filter-hp input{
+        -webkit-appearance: none;  
+        appearance: none;
+        width: 100%; 
+        height: 10px; 
+        background: #1c1c36; 
+        border-radius: 5px;
+        position: absolute;
+        pointer-events: none;
+        opacity: 1;
+        top: 20px;
+    }
+    .filter-hp input:focus{
+        outline: none;
+    }
+    .filter-hp .filter-min{
+        width: 92%;
+    }
+
+    .filter-hp .filter-max{
+        width: 92%;
+        right: 0;
+    }
+    .filter-hp input::-webkit-slider-thumb {
+        -webkit-appearance: none; 
+        appearance: none;
+        width: 15px; 
+        height: 15px; 
+        background: var(--color-third); 
+        filter: opacity(.7);
+        cursor: pointer;
+        border-radius: 50%;
+        -webkit-transition: .1s; 
+        transition: border .1s;
+        position: relative;
+        z-index: 1;
+        outline: 2px solid var(--color-first);
+        pointer-events: all;
+    }
+
+    .filter-hp .filter-min::-webkit-slider-thumb{
+        background-color: var(--color-second);
+    }
+
+    .filter-hp input::-webkit-slider-thumb:hover {
+        filter: opacity(1); 
+        scale: 1.1; 
+    }
+
+
+
 </style>
